a658f1f6bedcc6dade0a882cee11e68ad9477750
Tom Erik Støwer <testower@gmail.com>
2020-08-05 20:42:44 +0200

    Cleanup

--- tools/sonar_scanner_install.sh
+++ tools/sonar_scanner_install.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-4.0.0.1744
 
 set -euo pipefail
 
 VER="4.4.0.2170"
 
-
-
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${VER}-linux.zip
 unzip sonar-scanner-cli-${VER}-linux.zip
 rm sonar-scanner-cli-${VER}-linux.zip
-
 
 PATH="$(pwd)/sonar-scanner-${VER}-linux/bin:${PATH}"
 export PATH="${PATH}"
